7281393c989a53e4281243d0dc9319bc9545984f
LEMAIRE Yann <ylemaire@et.esiea.fr>
2024-01-09 09:35:11 +0100

    Implemented Field of view for enemy detection

---
+++ Assets/FieldOfView.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class FieldOfView : MonoBehaviour
+{
+    //GameObject of the entity
+    public GameObject m_gameObject;
+    //Position of the eyes for the fov
+    public Transform m_eyePosition;
+    //Layer that represents the targets that can be detected
+    public LayerMask m_targetLayerMask;
+    //Layer that represents the obstacles for the vision
+    public LayerMask m_obstructionLayerMask;
+    //Angle of view
+    [Range(0f, 360f)]
+    public float m_viewAngle;
+    //Radius of detection
+    public float m_viewRadius;
+    //Serialized bool to see in the editor if the target is detected
+    [SerializeField] private bool m_targetDetected = false;
+    //In case the entity isn't able 
+    [SerializeField] private bool m_canDetectTarget = true;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (!m_gameObject)
+            m_gameObject = this.gameObject;
+        if (!m_eyePosition)
+            m_eyePosition = this.transform;
+        StartCoroutine(FOVRoutine());
+    }
+
+    private void DetectTarget()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(m_eyePosition.position, m_viewRadius, m_targetLayerMask);
+        if (rangeChecks.Length == 0)
+        {
+            m_targetDetected = false;
+            return;
+        }
+        Transform targetTransform = rangeChecks[0].transform;
+        Vector3 toTargetDirection = (targetTransform.position - m_eyePosition.position).normalized;
+        var angle = Vector3.Angle(transform.forward, toTargetDirection);
+        Debug.Log("Angle : " + angle + " TargetDirection : " + toTargetDirection);
+        if (angle > (m_viewAngle / 2))
+        {
+            Debug.Log("target is out of view by angle");
+            m_targetDetected = false;
+            return;
+        }
+
+        float toTargetDistance = Vector3.Distance(m_eyePosition.position, targetTransform.position);
+        m_targetDetected = !Physics.Raycast(m_eyePosition.position, toTargetDirection, toTargetDistance, m_obstructionLayerMask);
+    }
+
+    private void Update()
+    {
+        if (!m_targetDetected)
+            return;
+        Debug.Log("TARGET DETECTED ! ");
+    }
+
+    IEnumerator FOVRoutine()
+    {
+        WaitForSeconds waitingTime = new WaitForSeconds(0.2f);
+        while (true)
+        {
+            yield return waitingTime;
+            if (!m_canDetectTarget)
+            {
+                m_targetDetected = false;   
+                continue;
+            }
+            DetectTarget();
+        }
+    }
+}
